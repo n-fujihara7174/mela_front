7f428a555015dabf656f9c535fd8106eb53c31ed
n-fujihara7174 <n.fujihara7174@gmail.com>
2021-12-23 08:14:33 +0900

    cssを追加

--- app/src/components/adminPage/user/UserList.vue
+++ app/src/components/adminPage/user/UserList.vue
@@ -1,34 +1,41 @@
 <template>
-  <div id="UserList" class="user-list">
-    <h2>ユーザー一覧</h2>
+  <div id="UserList" class="mx-xl-5">
+    <div class="mb-5">
+      <h2>ユーザー一覧</h2>
+    </div>
     <div class="user-list-wrapper">
-      <div class="serch-wrapper">
-        <div class="serch-user-name">
-          <label>ユーザーID、メールアドレス</label>
+      <div class="serch-wrapper row">
+        <div class="serch-user-name col-1">
+          <label class="search-parameter-label small">ユーザーID、メールアドレス</label>
           <input v-model="refState.searchValue.userIdOrEmail" />
-          <button @click="searchUsers">送信</button>
+        </div>
+        <div class="col-xxl-2">
+          <button @click="searchUsers">検索</button>
         </div>
       </div>
-      <table>
-        <tr class="table-header">
-          <th class="id">id</th>
-          <th class="user-name">ユーザー名</th>
-          <th class="user-id">ユーザーID</th>
-          <th class="self-introduction">自己紹介</th>
-          <th class="email">メールアドレス</th>
-          <th class="phone-number">電話番号</th>
-          <th class="birthday">生年月日</th>
-          <th class="image">画像</th>
-          <th class="post-count">投稿数</th>
-          <th class="like">いいね通知</th>
-          <th class="comment">コメント通知</th>
-          <th class="message">メッセージ通知</th>
-          <th class="calender">カレンダー通知</th>
-          <th class="is_delete">削除</th>
-          <th class="created-at">登録日時</th>
-          <th class="updated-at">更新日時</th>
-        </tr>
-        <tr v-for="(user, index) in refState.users" :key="index">
+      <table class="table table-striped">
+        <thead>
+          <tr class="table-header">
+            <th class="id">id</th>
+            <th class="user-name">ユーザー名</th>
+            <th class="user-id">ユーザーID</th>
+            <th class="self-introduction">自己紹介</th>
+            <th class="email">メールアドレス</th>
+            <th class="phone-number">電話番号</th>
+            <th class="birthday">生年月日</th>
+            <th class="image">画像</th>
+            <th class="post-count">投稿数</th>
+            <th class="like">いいね通知</th>
+            <th class="comment">コメント通知</th>
+            <th class="message">メッセージ通知</th>
+            <th class="calender">カレンダー通知</th>
+            <th class="is_delete">削除</th>
+            <th class="created-at">登録日時</th>
+            <th class="updated-at">更新日時</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in refState.users" :key="index">
           <td>{{ user.id }}</td>
           <td>{{ user.user_name }}</td>
           <td>{{ user.user_id }}</td>
@@ -46,6 +53,7 @@
           <td>{{ user.created_at }}</td>
           <td>{{ user.updated_at }}</td>
         </tr>
+        </tbody>
       </table>
     </div>
   </div>
@@ -124,5 +132,41 @@ export default defineComponent({
 </script>
 
 <style scoped>
-@import "../../../css/adminPage/user/listStyle.css";
+tr,th,td {
+  border-collapse: collapse;
+  border:1px solid;
+  border-color: #d3d3d3;
+  padding: 10px;
+}
+
+.user-list {
+  margin-left: 60px;
+  margin-right: 60px;
+}
+
+.user-list-wrapper {
+  width: 2500px;
+}
+
+.table-header {
+  background-color: #666666;
+  color: #DCDCDC;
+}
+
+.id {
+  width: 80px;
+}
+
+.user-name, .display-user-id, .phone-number, .self-introduction, .email, .image{
+  width: 200px;
+}
+
+.birthday {
+  width: 110px;
+}
+
+.search-parameter-label {
+  display: block;
+}
+
 </style>
