727a70cae42274476bfde9e0b5ef918ae1cec593
n-fujihara7174 <n.fujihara7174@gmail.com>
2022-06-12 19:04:31 +0900

    詳細画面のモック完成

--- app/src/components/template/main/Main.vue
+++ app/src/components/template/main/Main.vue
@@ -1,51 +1,217 @@
 <template>
-  <div class="photos">
-    <img
-      src="@/assets/1BCD5A44-7493-40F9-810C-5D702DF37948.jpeg"
-      alt=""
-      class="w-1/5 mb-4 mb-4"
-    />
-    <img src="@/assets/22652726_l.jpg" alt="" class="w-1/5 mb-4" />
-    <img
-      src="@/assets/459412C0-F0CA-4B3C-AEC7-292375082F5E.jpeg"
-      alt=""
-      class="w-1/5 mb-4"
-    />
-    <img
-      src="@/assets/70530182-F7B7-4017-8E91-82891357514D.jpeg"
-      alt=""
-      class="w-1/5 mb-4"
-    />
-    <img src="@/assets/background_00024.jpeg" alt="" class="w-1/5 mb-4" />
-    <img
-      src="@/assets/D85E77DD-3A38-4238-B2F2-4A685BF44E75.jpeg"
-      alt=""
-      class="w-1/5 mb-4"
-    />
-    <img
-      src="@/assets/EE4BB711-17D7-4FFE-B974-E10B34840B8C.jpeg"
-      alt=""
-      class="w-1/5 mb-4"
-    />
-    <img
-      src="@/assets/EE5C82CE-3669-479A-9639-243211AE6430.jpeg"
-      alt=""
-      class="w-1/5 mb-4"
-    />
-    <img src="@/assets/IMG_0629.jpeg" alt="" class="w-1/5 mb-4" />
-    <img src="@/assets/IMG_0695.jpeg" alt="" class="w-1/5 mb-4" />
-    <img src="@/assets/メロンちゃん.png" alt="" class="w-1/5 mb-4" />
+  <div class="photos p-4">
+    <!-- 一つのコンテンツ -->
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img
+          src="@/assets/1BCD5A44-7493-40F9-810C-5D702DF37948.jpeg"
+          alt=""
+          class="w-full"
+        />
+        <div class="bg-white w-full p-4">
+          <div class="mb-2">
+            <p class="text-gray-800 text-xl font-medium">メロンちゃんの画像</p>
+            <p class="text-gray-500 text-sm">
+              By
+              <a href="#" class="text-indigo-600 hover:underline"
+                >n-fujihara
+              </a>
+            </p>
+          </div>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+
+    <!-- 一つののコンテンツ -->
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img src="@/assets/22652726_l.jpg" alt="" class="w-full" />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img
+          src="@/assets/459412C0-F0CA-4B3C-AEC7-292375082F5E.jpeg"
+          alt=""
+          class="w-full"
+        />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img
+          src="@/assets/70530182-F7B7-4017-8E91-82891357514D.jpeg"
+          alt=""
+          class="w-full"
+        />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img src="@/assets/background_00024.jpeg" alt="" class="w-full" />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img
+          src="@/assets/D85E77DD-3A38-4238-B2F2-4A685BF44E75.jpeg"
+          alt=""
+          class="w-full"
+        />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img
+          src="@/assets/EE4BB711-17D7-4FFE-B974-E10B34840B8C.jpeg"
+          alt=""
+          class="w-full"
+        />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img
+          src="@/assets/EE5C82CE-3669-479A-9639-243211AE6430.jpeg"
+          alt=""
+          class="w-full"
+        />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img src="@/assets/IMG_0629.jpeg" alt="" class="w-full" />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img src="@/assets/IMG_0695.jpeg" alt="" class="w-full" />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="overflow-hidden shadow-lg rounded-lg cursor-pointer mb-4">
+      <a href="#" class="w-full block" @click="transitionDetail">
+        <img src="@/assets/メロンちゃん.png" alt="" class="w-full" />
+        <div class="bg-white w-full p-4">
+          <p class="text-gray-800 text-xl font-medium mb-2">
+            New Mac is here !
+          </p>
+          <p class="text-gray-400 font-light text-md">
+            The new supermac is here, 543 cv and 140 000$. This is best racing
+            computer about 7 years on...
+          </p>
+        </div>
+      </a>
+    </div>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import { useRouter } from "vue-router";
+
+export default defineComponent({
+  setup() {
+    const router = useRouter();
+
+    // イベントハンドルする関数なので、イベントの変更を検知できる
+    // このイベントにてファイル情報を取得している
+    const transitionDetail = () => {
+      router.push("Detail");
+    };
+    return { transitionDetail };
+  },
+});
+</script>
 
 <style scoped>
 .photos {
   -webkit-column-count: 4;
   -moz-column-count: 4;
   column-count: 4;
-}
-.photos img {
-  /* Just in case there are inline attributes */
-  width: 100% !important;
 }
 </style>
--- app/src/components/template/main/WorkDetail.vue
+++ app/src/components/template/main/WorkDetail.vue
@@ -1,128 +1,86 @@
 <template>
-  <div>
-    <div class="bg-gray-800 w-full py-10 flex justify-center">
-      <div class="w-1/12 grid place-items-center">
-        <div
-          v-if="refState.imageIndex !== 0"
-          class="w-16 h-16 rounded-full grid place-items-center"
-          :class="{ 'bg-gray-600': refState.mouseover_left_arrow }"
-          @mouseover="mouseover_left_arrow"
-          @mouseleave="mouseleave_left_arrow"
-          @click="preImage"
-        >
-          <div class="w-8">
-            <img src="@/assets/左矢印.svg" alt="" />
-          </div>
-        </div>
-        <div v-else></div>
-      </div>
-      <div class="w-1/2">
+  <div class="flex">
+    <!-- image section  -->
+    <div class="w-7/12 pt-10">
+      <!-- main image -->
+      <div class="h-128 flex justify-center">
         <img
           :src="importImage(refState.displayImage)"
           alt=""
-          class="w-full h-full"
+          class="w-10/12 object-scale-down"
         />
       </div>
-      <div class="w-1/12 grid place-items-center">
-        <div
-          v-if="refState.imageIndex !== 3"
-          class="w-16 h-16 rounded-full grid place-items-center"
-          :class="{ 'bg-gray-600': refState.mouseover_right_arrow }"
-          @mouseover="mouseover_right_arrow"
-          @mouseleave="mouseleave_right_arrow"
-          @click="nextImage"
-        >
-          <div class="w-8">
-            <img src="@/assets/右矢印.svg" alt="" class="rotate-45" />
-          </div>
-        </div>
-        <div v-else></div>
-      </div>
-    </div>
 
-    <div class="bg-gray-800 w-full py-10 flex justify-center">
-      <div class="w-1/2">
+      <!-- image select -->
+      <div class="pt-5 flex justify-center">
         <div class="flex justify-around">
-          <img
-            v-for="(image, index) in refState.imageList"
-            :key="index"
-            :src="importImage(image)"
-            alt=""
-            class="w-1/6"
-            :class="{
-              'border-solid border-8 border-green-600':
-                image === refState.displayImage,
-            }"
-          />
+          <div
+            v-for="(image, key, index) in refState.imageList"
+            :key="key"
+            class="w-24 mx-2"
+          >
+            <img
+              :src="importImage(image)"
+              alt=""
+              class="h-24 object-cover"
+              :class="{
+                'border-solid border-2 border-green-600':
+                  image === refState.displayImage,
+              }"
+              @click="selectImage(index)"
+            />
+          </div>
         </div>
       </div>
     </div>
 
-    <div class="flex justify-center pt-20 pb-96">
-      <div class="flex justify-around w-8/12">
-        <div class="w-5/12 rounded overflow-hidden shadow-lg p-5">
-          <div class="flex justify-between">
-            <div>
-              <img
-                src="@/assets/ユーザーアイコン.svg"
-                alt=""
-                class="w-10 h-10 rounded-full mr-4 inline-block"
-              />
-              <span class="align-middle font-bold text-xl mb-2"
-                >naoki.fujihara</span
-              >
-            </div>
-            <div>
-              <button
-                type="button"
-                class="text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-600 dark:focus:ring-blue-800 inline-block"
-              >
-                フォロー
-              </button>
-            </div>
-          </div>
-          <div class="mt-4">
-            ポートフォリオを作成しています。<br />
-            普段はゲームをしています
-          </div>
-          <div class="px-6 py-4 text-right"></div>
-        </div>
-        <div class="p-5">
-          <div class="">
-            <button
-              class="text-white px-4 w-auto h-10 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
-            >
-              <svg
-                viewBox="0 0 20 20"
-                enable-background="new 0 0 20 20"
-                class="w-6 h-6 inline-block mr-1"
-              >
-                <path
-                  fill="#FFFFFF"
-                  d="M17.19,4.155c-1.672-1.534-4.383-1.534-6.055,0L10,5.197L8.864,4.155c-1.672-1.534-4.382-1.534-6.054,0
-                                    c-1.881,1.727-1.881,4.52,0,6.246L10,17l7.19-6.599C19.07,8.675,19.07,5.881,17.19,4.155z"
-                />
-              </svg>
-              <span>好き!</span>
-            </button>
-          </div>
-          <div class="mt-5">
-            <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-              >#クランウェルツノガエル</span
-            >
-            <span
-              class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-              >#テラリウム</span
-            >
-          </div>
-          <div>
-            {{ refState.contents }}
-          </div>
+    <!-- detail info section -->
+    <div class="w-4/12 pt-10">
+      <div class="flex flex-col md:flex-row -mx-4 h-64">
+        <div class="md:flex-1 px-4">
+          <h2
+            class="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl"
+          >
+            メロンちゃんの画像
+          </h2>
+          <p class="text-gray-500 text-sm">
+            By
+            <a href="#" class="text-indigo-600 hover:underline">n-fujihara </a>
+          </p>
+          <p class="text-gray-500 pt-5">
+            うちで飼育しているクランウェルツノガエルのメロンちゃんです。
+          </p>
         </div>
       </div>
+      <div class="">
+        <button
+          class="text-white px-4 w-auto h-10 bg-red-600 rounded-full hover:bg-red-700 active:shadow-lg mouse shadow transition ease-in duration-200 focus:outline-none"
+        >
+          <svg
+            viewBox="0 0 20 20"
+            enable-background="new 0 0 20 20"
+            class="w-6 h-6 inline-block mr-1"
+          >
+            <path
+              fill="#FFFFFF"
+              d="M17.19,4.155c-1.672-1.534-4.383-1.534-6.055,0L10,5.197L8.864,4.155c-1.672-1.534-4.382-1.534-6.054,0
+                                    c-1.881,1.727-1.881,4.52,0,6.246L10,17l7.19-6.599C19.07,8.675,19.07,5.881,17.19,4.155z"
+            />
+          </svg>
+          <span>好き!</span>
+        </button>
+      </div>
+      <div class="mt-5">
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#クランウェルツノガエル</span
+        >
+        <span
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          >#テラリウム</span
+        >
+      </div>
     </div>
-    <img src="1BCD5A44-7493-40F9-810C-5D702DF37948.jpeg" alt="" />
   </div>
 </template>
 
@@ -164,31 +122,9 @@ export default defineComponent({
     const imageArray = Object.values(refState.imageList);
     console.log(imageArray);
 
-    const nextImage = () => {
-      refState.imageIndex += 1;
-      refState.displayImage = imageArray[refState.imageIndex];
-    };
-
-    const preImage = () => {
-      refState.imageIndex -= 1;
-      refState.displayImage = imageArray[refState.imageIndex];
-    };
-
-    const mouseover_left_arrow = () => {
-      console.log("mouseover_left_arrow");
-      refState.mouseover_left_arrow = true;
-    };
-
-    const mouseover_right_arrow = () => {
-      refState.mouseover_right_arrow = true;
-    };
-
-    const mouseleave_left_arrow = () => {
-      refState.mouseover_left_arrow = false;
-    };
-
-    const mouseleave_right_arrow = () => {
-      refState.mouseover_right_arrow = false;
+    const selectImage = (index: number) => {
+      console.log("index:" + index);
+      refState.displayImage = imageArray[index];
     };
 
     const importImage = (imagePath: string) => {
@@ -198,12 +134,7 @@ export default defineComponent({
     return {
       refState,
       importImage,
-      nextImage,
-      preImage,
-      mouseover_left_arrow,
-      mouseover_right_arrow,
-      mouseleave_left_arrow,
-      mouseleave_right_arrow,
+      selectImage,
     };
   },
 });
